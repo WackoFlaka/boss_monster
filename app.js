bf6dc7435c66151888c9843c452ac3db45b33ab6
Joaquin Chavez <jchav5601@gmail.com>
2024-02-02 08:10:51 -0700

    COMPLETED BOSS_MONSTER

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 let gold = 0
-let reward = 10
 let potions = 0
-
-
-const player = {
-    name: 'Knight',
-    type: 'Human',
-    damage: 10,
-    health: 50
-}
+let potionEffect = 10
+let reward = 10
 
 const heroes = [
     {
+        name: 'Knight',
+        type: 'Human',
+        damage: 10,
+        health: 50
+    },
+    {
         name: 'Sam',
         type: 'dwarf',
+        isEnabled: false,
         damage: 5,
-        health: 100,
+        health: 100
     },
     {
         name: 'Jake',
         type: 'elf',
+        isEnabled: false,
         damage: 10,
-        health: 50,
+        health: 50
     }
 ]
 
@@ -32,143 +33,179 @@ const boss = {
     level: 1
 }
 
-// function bossLevelIncrease(){
-//     const bossHealth = 10
-//     const bossMaxHealth = 10
-//     const bossDamage = 1
-//     const bossLevel = 1
-    
-//      if(boss.level++){
-//         let boss.Health = 
-//      }
-
-
-
-
-function drawPlayer() {
-    const name = document.getElementById('playerName')
-    const health = document.getElementById('playerHealth')
-    const damage = document.getElementById('playerDamage')
-    
-    
-    name.innerText = player.name
-    health.innerText = player.health
-    damage.innerText = player.damage
-    document.getElementById('playerGold').innerHTML = gold
-}
-
-function drawSam() {
-    if(gold >= 100) {
-    const name = document.getElementById('heroName1')
-    const health = document.getElementById('heroHealth1')
-    const damage = document.getElementById('heroDamage1')
-    document.getElementById('hero-1').style.display = 'block'
-    
-    const foundSam = heroes.find(hero => hero.name = 'Sam')
-    name.innerHTML = foundSam.name
-    health.innerHTML = foundSam.health
-    damage.innerHTML = foundSam.damage
-    setInterval(samAttack, 1000)
-    samAttack() 
-    }
-}
-
-function drawJake() {
-    if(gold > 1000) {
-        const name = document.getElementById('heroName2')
-        const health = document.getElementById('heroHealth2')
-        const damage = document.getElementById('heroDamage2')
-        document.getElementById('hero-2').style.display = 'block'
-        
-        const foundJake = heroes.find(hero => hero.name = 'Jake')
-        name.innerHTML = foundJake.name
-        health.innerHTML = foundJake.health
-        damage.innerHTML = foundJake.damage
-        setInterval(samAttack, 1000)
-        jakeAttack()
-    }
-}
-
-function usePotion() {
-    if(potions >= 0) {
-        potions -= 1
-        player.health += 10
-        document.getElementById('potion').innerHTML = potions
-        drawPlayer()
-    }
-}
-
-function buyPotion() {
+function drawHero() {
+    heroes.forEach(hero => {
+        const heroElem = document.getElementById(hero.name)
+        const statsElem = heroElem.querySelector('.stats')
+        statsElem.innerText = `
+        ${hero.name}
+        Damage: ${hero.damage}
+        Health: ${hero.health}
+        `
+    })
+    document.getElementById('gold').innerHTML = gold
     document.getElementById('potion').innerHTML = potions
-    
-    
-    if(gold > 25) {
-        gold -= 25
-        potions += 1
-        document.getElementById('potion').innerHTML = potions
-        drawPlayer()
-        checkPotion()
-    } 
+    document.getElementById('effect').innerHTML = potionEffect
 }
 
-        
-     
-function playerAttack() {
-    boss.health -= player.damage
-    console.log(boss.health)
-    nextLevel()
+function attackBoss() {
+    const knight = heroes.find(hero => hero.name == 'Knight')
+    boss.health -= knight.damage
     callHealthBar()
+    nextLevel()
 }
 
-function samAttack() { 
-    const team = heroes.find(hero => hero.name == 'Sam')
-    boss.health -= team.damage
-    console.log(boss.health)
-    nextLevel()
-    callHealthBar()
-}
-
-function jakeAttack() {
+function heroesAttack() {
+    const sam = heroes.find(hero => hero.name == 'Sam')
+    const jake = heroes.find(hero => hero.name == 'Jake')
     
-    const team = heroes.find(hero => hero.name == 'Jake')
-    boss.health -= team.damage
-    console.log(boss.health)
-    nextLevel()
-    callHealthBar()
+    if(sam.isEnabled) {
+        boss.health -= sam.damage
+        callHealthBar()
+        nextLevel()
+    }
+    
+    if(jake.isEnabled) {
+        boss.health -= jake.damage
+        callHealthBar()
+        nextLevel()
+    }
 }
 
 function bossAttack() {
-    const team = heroes.filter(hero => hero.health)
-    team.forEach(heros => heros.health -= boss.damage)
-    player.health -= boss.damage
-    drawPlayer()
+    const sam = heroes.find(hero => hero.name == 'Sam')
+    const jake = heroes.find(hero => hero.name == 'Jake')
+    const knight = heroes.find(hero => hero.name == 'Knight')
+    knight.health -= boss.damage
+    drawHero()
+    
+    if(sam.isEnabled) {
+        sam.health -= boss.damage
+        drawHero()
+    }
+    
+    if(jake.isEnabled) {
+        jake.health -= boss.damage
+        drawHero()
+    }
+    
+    if(knight.health <= 0) {
+        window.alert("GAME OVER")
+        gameOver()
+    }
+}
+
+function buyCharacter(name, num) {
+    const heroName = heroes.find(hero => hero.name == name)
+    
+    if(heroName.name == 'Sam' && gold >= num) {
+        heroName.isEnabled = true
+        console.log('You bought Sam!')
+        gold -= num
+        document.getElementById('Sam').style.display = 'block'
+        document.getElementById('upgradeSam').style.display = 'block'
+        drawHero()
+        heroesAttack()
+    } else {
+        console.log("Something went wrong...")
+    }
+    
+    if(heroName.name == 'Jake' && gold >= num) {
+        heroName.isEnabled = true
+        document.getElementById('Jake').style.display = 'block'
+        document.getElementById('upgradeJake').style.display = 'block'
+        gold -= num
+        console.log('You bought Jake!')
+        drawHero()
+        heroesAttack()
+    }
+}
+
+//function upgrade(name, cost) {
+ //   const heroName = heroes.find(hero => hero.name == name)
+  //  
+  //  if(heroName.name == 'Sam' && heroName.isEnabled == true && gold >= cost) {
+ //       heroName.damage += 10
+  //      cost += 5
+   //     document.getElementById('upgradeSam').innerHTML = cost
+ //   }
+//}
+
+function buyPotion() {
+    if(gold >= 25) {
+        potions++
+        gold -= 25
+        drawHero()
+    }
+}
+
+function applyPotion(name) {
+    const heroName = heroes.find(hero => hero.name == name)
+    if(potions > 0) {
+        heroName.health += potionEffect
+        potions--
+        drawHero()
+    }
 }
 
 function nextLevel() {
+    const knight = heroes.find(hero => hero.name == 'Knight')
     const level = document.getElementById('bossLevel')
     document.getElementById('reward').innerHTML = reward
-
     
     if(boss.health <= 0) {
         gold += reward
         reward += 10
         boss.level++
         boss.maxHealth += 20
+        boss.damage += 2
         boss.health = boss.maxHealth
+        potionEffect += 2
+        knight.damage += 2
         level.innerText = boss.level
-        player.damage += 5
         document.getElementById('reward').innerHTML = reward
-        drawPlayer()
-    }
+        document.getElementById('bossBar').style.width = `100%`
+        drawHero()
+    } 
 }
 
 function callHealthBar(){
-document.getElementById('bossBar').style.width = `${boss.health/boss.maxHealth*100}%`
-
+    document.getElementById('bossBar').style.width = `${(boss.health/boss.maxHealth)*100}%`
+    }
+    
+    
+function reset() {
+    gold = 0
+    potions = 0
+    potionEffect = 10
+    reward = 10
+    boss.health = 100
+    boss.maxHealth = 100
+    boss.damage = 5
+    boss.level = 1
+    document.getElementById('bossLevel').innerHTML = boss.level
+    drawHero()
+    callHealthBar()
+    const sam = heroes.find(hero => hero.name == 'Sam')
+    const jake = heroes.find(hero => hero.name == 'Jake')
+    const knight = heroes.find(hero => hero.name == 'Knight')
+    
+    knight.damage = 10
+    knight.health = 50
+    sam.isEnabled = false
+    jake.isEnabled = false 
+    document.getElementById('Jake').style.display = 'none'
+    document.getElementById('Sam').style.display = 'none'
 }
 
-//nextLevel()
-drawPlayer()
-// ANCHOR time intervals
+function gameOver() {
+    reset()
+}
 
-//setInterval(bossAttack, 5000)
+function updateScreen() {
+    drawHero()
+    setInterval(heroesAttack, 1000)
+    setInterval(bossAttack, 5000)
+}
+
+updateScreen()
